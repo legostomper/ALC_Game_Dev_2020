bf218e448e718e7fe067790a2b31d0eb75fcf558
Mitchell <mitchell.p031@stu.nebo.edu>
2019-10-08 10:29:04 -0600

    prototype 2
    started prototype 2

---
+++ Create_With_Code/prototype 2/Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float xRange = 20f;
+    public float horizontalInput;
+    public float speed = 50f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < -20)
+        {
+            transform.position = new Vector3(-20, transform.position.y, transform.position.z);
+
+        }
+
+        if (transform.position.x > 20)
+        {
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+}
