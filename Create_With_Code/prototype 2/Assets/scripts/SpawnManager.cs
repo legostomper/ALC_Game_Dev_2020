c854721bc1bda92c09adcf50094cd244a85d913b
Mitchell <mitchell.p031@stu.nebo.edu>
2019-10-16 10:26:17 -0600

    created random position spawning

--- Create_With_Code/prototype 2/Assets/scripts/SpawnManager.cs
+++ Create_With_Code/prototype 2/Assets/scripts/SpawnManager.cs
@@ -3,22 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
-{
 
-    public GameObject[] animalPrefabs;
+
+
+{
+private float spawnRangeX = 20;
+private float spawnPosZ = 20;
+public GameObject[] animalPrefabs;
+private float startDelay = 0f;
+    private float spawnInterval = 1f;
+
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);  
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+
+    }
+        void SpawnRandomAnimal()
         {
+
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+                            0, spawnPosZ);
+
             int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
+                animalPrefabs[animalIndex].transform.rotation);
 
         }
 
 
-
-    }
+    
 }
