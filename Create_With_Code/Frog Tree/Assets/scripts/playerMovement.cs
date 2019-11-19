d3a829ee8389e33a6599f0efc13819b83abf4956
Mitchell <mitchell.p031@stu.nebo.edu>
2019-11-19 10:28:46 -0700

    worked on private project

--- Create_With_Code/Frog Tree/Assets/scripts/playerMovement.cs
+++ Create_With_Code/Frog Tree/Assets/scripts/playerMovement.cs
@@ -4,29 +4,75 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    private Rigidbody playerRb;
+
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    public float jumpForce = 10;
+    public float speed = 0.1f;
     public float rotationSpeed = 5f;
     public float HorizontalInput;
+    public float turnSpeed = 10f;
+    public float back = 0.05f;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+        
 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        // get the user's vertical input
-        HorizontalInput = Input.GetAxis("Vertical");
 
-        // move the plane forward at a constant rate
+
+        // move the frog forward and back
         if (Input.GetKey(KeyCode.W))
-           {
-            transform.Translate(Vector3.forward * speed);
+        {
+            transform.Translate(Vector3.left * speed);
         }
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.left * Time.deltaTime * HorizontalInput * rotationSpeed);
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.right * back);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isOnGround && !gameOver)
+        {
+
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+
+        void OnCollisionEnter(Collision other)
+        {
+
+            if (other.gameObject.CompareTag("BadBoi"))
+            {
+
+                Debug.Log("Game Over!");
+
+                Destroy(other.gameObject);
+
+            }
+
+
+        }
     }
 }
 
